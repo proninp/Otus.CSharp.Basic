32d5fe59b497a856619cedb5e8be60fc0b9a7798
proninp <velninpapro@gmail.com>
2024-08-31 20:15:01 +0300

    Lesson 05 - HomeWork 01 - [2024.08.31]

---
+++ HomeWorks/05.HomeWork.01/HomeWork01/HomeWork01/Program.cs
@@ -0,0 +1,14 @@
+﻿static void WriteTable(int n, string s)
+{
+    if (n < 1 || n > 6)
+    {
+        Console.WriteLine("Введите число от 1 до 6");
+        return;
+    }
+    if (string.IsNullOrEmpty(s))
+    {
+        Console.WriteLine("Текст не может быть пустым");
+        return;
+    }
+
+}
