75d6f20313d73958b1e3f32a03b802f8de2af778
proninp <proninpaul@yandex.ru>
2025-01-23 11:36:50 +0300

    Lesson 37 - HomeWork 12 - Librarian

---
+++ HomeWorks/37.HomeWork.12/HomeWork12/HomeWork12.Librarian/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace HomeWork12.Librarian;
+public enum Menu
+{
+    [Description("1 - Добавить книгу")]
+    AddABook,
+    [Description("2 - Вывести список непрочитанного")]
+    ShowUnread,
+    [Description("3 - Выйти")]
+    Quit
+}
---
+++ HomeWorks/37.HomeWork.12/HomeWork12/HomeWork12.Librarian/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Concurrent;
+using HomeWork12.Librarian;
+using Spectre.Console;
+
+var library = new ConcurrentDictionary<string, int>();
+
+var task = Task.Run(CalculatePercents);
+
+ShowMenu();
+
+void ShowMenu()
+{
+    while (true)
+    {
+        var choice = AnsiConsole.Prompt(
+            new SelectionPrompt<Menu>()
+                .Title("Выберите действие:")
+                .AddChoices(Enum.GetValues<Menu>())
+                .UseConverter(option => option.GetDescription()));
+
+
+        switch (choice)
+        {
+            case Menu.AddABook:
+                AddNewBook();
+                break;
+
+            case Menu.ShowUnread:
+                ShowLibrary();
+                break;
+
+            case Menu.Quit:
+                AnsiConsole.MarkupLine("[green]До свидания![/]");
+                return;
+
+            default:
+                AnsiConsole.MarkupLine("[red]Неверный выбор![/]");
+                break;
+        }
+        Continue();
+    }
+}
+
+void AddNewBook()
+{
+    var title = AnsiConsole.Ask<string>("[yellow]Введите название книги:[/]");
+    if (library.TryAdd(title, 0))
+        AnsiConsole.MarkupLine($"[green]Книга \"{title}\" добавлена![/]");
+}
+
+void ShowLibrary()
+{
+    foreach (var kvp in library)
+        AnsiConsole.MarkupLine($"[cyan]\"{kvp.Key}\"[/] - {kvp.Value}%");
+}
+
+void Continue()
+{
+    AnsiConsole.MarkupLine($"{Environment.NewLine}Нажмите любую клавишу для продолжения...");
+    Console.ReadKey(true);
+    Console.Clear();
+}
+
+async Task CalculatePercents()
+{
+    while (true)
+    {
+        Parallel.ForEach(library, (kvp) => 
+        {
+            if (kvp.Value < 100)
+                library[kvp.Key]++;
+            else
+                library.TryRemove(kvp);
+        });
+        await Task.Delay(TimeSpan.FromSeconds(1));
+    }
+}
