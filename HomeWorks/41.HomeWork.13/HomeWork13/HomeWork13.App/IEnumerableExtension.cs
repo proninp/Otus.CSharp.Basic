2fe71e79bcae1d749ca0ce3d3ffd92e1eefe020d
proninp <proninpaul@yandex.ru>
2025-02-03 08:35:51 +0300

    Lesson 41 - HomeWork 13 - Main App

---
+++ HomeWorks/41.HomeWork.13/HomeWork13/HomeWork13.App/IEnumerableExtension.cs
@@ -0,0 +1,20 @@
+﻿namespace HomeWork13;
+public static class IEnumerableExtension
+{
+
+    public static IEnumerable<T> Top<T>(this IEnumerable<T> source, int x)
+        where T : IComparable<T>
+    {
+        return source.Top(x, e => e);
+    }
+
+    public static IEnumerable<T> Top<T, K>(this IEnumerable<T> source, int x, Func<T, K> selector)
+    {
+        if (x is < 1 or > 100)
+            throw new ArgumentException($"{nameof(x)} must be greater than zero and less the or equal to 100");
+        
+        var elemsCount = (int)Math.Ceiling((double)source.Count() / 100 * x);
+        
+        return source.OrderByDescending(selector).Take(elemsCount);
+    }
+}
---
+++ HomeWorks/41.HomeWork.13/HomeWork13/HomeWork13.App/Program.cs
@@ -0,0 +1,20 @@
+﻿using HomeWork13;
+using HomeWork13.App;
+
+var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+var listPart = list.Top(30);
+Console.WriteLine(string.Join(", ", listPart));
+
+var persons = new List<Person>()
+{
+    new Person("Bjarne", 74),
+    new Person("Linus", 55),
+    new Person("James", 69),
+    new Person("Bill", 69),
+    new Person("Guido", 68),
+    new Person("Brendan", 63)
+};
+
+var personsPart = persons.Top(50, p => p.Age);
+Console.WriteLine(string.Join("\n", personsPart));
