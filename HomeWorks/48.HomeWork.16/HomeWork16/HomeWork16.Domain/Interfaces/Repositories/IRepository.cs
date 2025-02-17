e838f13092ba7df7c3fd5e4ee4385e7715b661f0
proninp <proninpaul@yandex.ru>
2025-02-17 09:59:44 +0300

    Lesson 48 - HomeWork 16 - Repositories

--- HomeWorks/48.HomeWork.16/HomeWork16/HomeWork16.Domain/Interfaces/Repositories/IRepository.cs
+++ HomeWorks/48.HomeWork.16/HomeWork16/HomeWork16.Domain/Interfaces/Repositories/IRepository.cs
@@ -1,15 +1,22 @@
 ﻿using HomeWork16.Domain.Models.Abstractions;
+using LinqToDB;
 
 namespace HomeWork16.Domain.Interfaces.Repositories;
 public interface IRepository<T>
     where T : BaseModel
 {
-    Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken);
+    Task<T?> GetByIdAsync(
+        int id,
+        Func<IQueryable<T>, ILoadWithQueryable<T, object>>? include = null,
+        CancellationToken cancellationToken = default);
 
-    Task<IEnumerable<T>> GetAsync(CancellationToken cancellationToken);
+    Task<IEnumerable<T>> GetAsync(
+        Func<IQueryable<T>, ILoadWithQueryable<T, object>>? include = null,
+        CancellationToken cancellationToken = default);
+
     Task<T> AddAsync(T model, CancellationToken cancellationToken);
 
-    Task<T> UpdateAsync(T model, CancellationToken cancellationToken);
+    Task<int> UpdateAsync(T model, CancellationToken cancellationToken);
 
-    Task DeleteAsync(int id, CancellationToken cancellationToken);
+    Task<int> DeleteAsync(int id, CancellationToken cancellationToken);
 }
---
+++ HomeWorks/48.HomeWork.16/HomeWork16/HomeWork16.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,62 @@
+﻿using HomeWork16.Domain.Interfaces.Repositories;
+using HomeWork16.Domain.Models.Abstractions;
+using HomeWork16.Infrastructure.Data;
+using LinqToDB;
+
+namespace HomeWork16.Infrastructure.Repositories;
+public sealed class Repository<T> : IRepository<T>
+    where T : BaseModel
+{
+    private readonly DatabaseContext _db;
+    private readonly ITable<T> _table;
+
+    public Repository(DatabaseContext db)
+    {
+        _db = db;
+        _table = _db.GetTable<T>();
+    }
+
+    public async Task<IEnumerable<T>> GetAsync(
+        Func<IQueryable<T>, ILoadWithQueryable<T, object>>? include = null,
+        CancellationToken cancellationToken = default)
+    {
+        var query = _table.AsQueryable();
+
+        if (include is not null)
+            query = include(query);
+
+        return await query.ToListAsync(cancellationToken);
+    }
+
+    public async Task<T?> GetByIdAsync(
+        int id,
+        Func<IQueryable<T>, ILoadWithQueryable<T, object>>? include = null,
+        CancellationToken cancellationToken = default)
+    {
+        var query = _table
+            .Where(x => x.Id == id);
+
+        if (include is not null)
+            query = include(query);
+
+        return await query.FirstOrDefaultAsync(cancellationToken);
+    }
+
+    public async Task<T> AddAsync(T model, CancellationToken cancellationToken)
+    {
+        model.Id = await _db.InsertWithInt32IdentityAsync(model, token: cancellationToken);
+        return model;
+    }
+
+    public async Task<int> UpdateAsync(T model, CancellationToken cancellationToken)
+    {
+        return await _db.UpdateAsync<T>(model, token: cancellationToken);
+    }
+
+    public async Task<int> DeleteAsync(int id, CancellationToken cancellationToken)
+    {
+        return await _table
+            .Where(x => x.Id == id)
+            .DeleteAsync(cancellationToken);
+    }
+}
